4100753bf38bf4244fcad53bc6799f520b7bfdda
Yan Malashonak <malashonock@gmail.com>
2022-04-10 12:14:51 +0300

    refactor: move main page slider adaptive configuration into separate module

---
+++ shelter/pages/main/js/slider.mjs
@@ -0,0 +1,52 @@
+import { pets } from "../../../shared/js/load-pets.mjs";
+import { PetCard } from "../../../shared/js/pet-card.mjs";
+import { shuffle } from "../../../shared/js/array-extensions.mjs";
+import { Slider } from "../../../shared/js/slider.mjs";
+
+export let slider;
+
+const breakpointDesktop = window.matchMedia("(min-width: 1280px)");
+const breakpointTablet = window.matchMedia(
+  "(min-width: 768px) and (max-width: 1279px)"
+);
+const breakpointMobile = window.matchMedia("(max-width: 767px)");
+
+const breakpoints = [
+  {
+    mediaQueryList: breakpointDesktop,
+    itemsPerPageRow: 3,
+  },
+  {
+    mediaQueryList: breakpointTablet,
+    itemsPerPageRow: 2,
+  },
+  {
+    mediaQueryList: breakpointMobile,
+    itemsPerPageRow: 1,
+  },
+];
+
+function getBreakPointChangedHandler(mediaQueryList, itemsPerPageRow) {
+  return function () {
+    if (mediaQueryList.matches) {
+      slider = new Slider({
+        sourceItems: pets,
+        itemsPerPageRow,
+        itemDOMGenerator: PetCard,
+      });
+    }
+  };
+}
+
+for (const breakpoint of breakpoints) {
+  const breakpointChangedHandler = getBreakPointChangedHandler(
+    breakpoint.mediaQueryList,
+    breakpoint.itemsPerPageRow
+  );
+
+  breakpointChangedHandler();
+
+  breakpoint.mediaQueryList.addEventListener("change", (event) =>
+    breakpointChangedHandler()
+  );
+}
